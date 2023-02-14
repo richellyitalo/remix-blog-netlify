7633aeede2619fbe6b679d712a4478f5291d19d2
Richelly Italo <richellyitalo@gmail.com>
2023-02-14 12:10:06 -0300

    add: credits

--- app/components/nav/AdminHeader.jsx
+++ app/components/nav/AdminHeader.jsx
@@ -5,53 +5,65 @@ export default function AdminHeader() {
   const userSession = useLoaderData();
 
   return (
-    <nav
-      id="site-header"
-      className="py-4 drop-shadow-md bg-slate-100 border p-2 px-5 rounded-md mb-4"
-    >
-      <ul className="list-inline flex">
-        <li className="mr-6">
-          <NavLink
-            className="text-blue-500 hover:text-blue-800"
-            to="/admin/posts"
-          >
-            Posts
-          </NavLink>
-        </li>
-        <li className="mr-6">
-          <NavLink
-            className="text-blue-500 hover:text-blue-800"
-            to="/admin/categories"
-            end
-          >
-            Categories
-          </NavLink>
-        </li>
-        <li className="mr-6">
-          <Form
-            method="post"
-            action="/logout"
-          >
-            <button className="text-white p-2 rounded-md bg-purple-500 hover:text-purple-200 hover:bg-purple-700">
-              Logout
-            </button>
-          </Form>
-        </li>
-        <li className="mr-6">
-          <Link
-            className="text-purple-500 p-2 hover:text-purple-800"
-            to="/"
-            target="_blank"
-          >
-            <AiOutlineRollback className="inline mr-1" />
-            go to Website
-          </Link>
-        </li>
-
-        <li className="break-all text-sm text-gray-500">
-          Hello, {userSession && userSession.email}
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav
+        id="site-header"
+        className="py-4 drop-shadow-md bg-slate-100 border p-2 px-5 rounded-md mb-0"
+      >
+        <ul className="list-inline flex">
+          <li className="mr-6">
+            <NavLink
+              className="text-blue-500 hover:text-blue-800"
+              to="/admin/posts"
+            >
+              Posts
+            </NavLink>
+          </li>
+          <li className="mr-6">
+            <NavLink
+              className="text-blue-500 hover:text-blue-800"
+              to="/admin/categories"
+              end
+            >
+              Categories
+            </NavLink>
+          </li>
+          <li className="mr-6">
+            <Form
+              method="post"
+              action="/logout"
+            >
+              <button className="text-white p-2 rounded-md bg-purple-500 hover:text-purple-200 hover:bg-purple-700">
+                Logout
+              </button>
+            </Form>
+          </li>
+          <li className="mr-6">
+            <Link
+              className="text-purple-500 p-2 hover:text-purple-800"
+              to="/"
+              target="_blank"
+            >
+              <AiOutlineRollback className="inline mr-1" />
+              go to Website
+            </Link>
+          </li>
+          <li className="break-all text-sm text-gray-500">
+            Hello, {userSession && userSession.email}
+          </li>
+        </ul>
+      </nav>
+      
+      <p className="text-right m-0">
+        <a
+          href="https://github.com/richellyitalo?tab=repositories&q=csgo-information"
+          rel="noreferrer"
+          target="_blank"
+          className="text-sm text-slate-100 hover:text-yellow-600 hover:no-underline"
+        >
+          by @richellyitalo
+        </a>
+      </p>
+    </>
   );
 }
--- app/components/nav/SiteHeader.jsx
+++ app/components/nav/SiteHeader.jsx
@@ -2,34 +2,50 @@ import { NavLink } from "react-router-dom";
 
 export default function SiteHeader() {
   return (
-    <nav id="site-header" className="py-4 bg-white border p-2 px-5 rounded-md mb-4">
-      <ul className="list-inline flex">
-        <li className="mr-6">
-          <NavLink
-            className="text-blue-500 hover:text-blue-800"
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="mr-6">
-          <NavLink
-            className="text-blue-500 hover:text-blue-800"
-            to="/categories"
-            end
-          >
-            Categories
-          </NavLink>
-        </li>
-        <li className="mr-6">
-          <NavLink
-            className="text-white p-2 rounded-md bg-purple-500 hover:text-purple-200 hover:bg-purple-700"
-            to="/auth"
-          >
-            Login
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav
+        id="site-header"
+        className="py-4 bg-white border p-2 px-5 rounded-md mb-0"
+      >
+        <ul className="list-inline flex">
+          <li className="mr-6">
+            <NavLink
+              className="text-blue-500 hover:text-blue-800"
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="mr-6">
+            <NavLink
+              className="text-blue-500 hover:text-blue-800"
+              to="/categories"
+              end
+            >
+              Categories
+            </NavLink>
+          </li>
+          <li className="mr-6">
+            <NavLink
+              className="text-white p-2 rounded-md bg-purple-500 hover:text-purple-200 hover:bg-purple-700"
+              to="/auth"
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <p className="text-right m-0">
+        <a
+          href="https://github.com/richellyitalo?tab=repositories&q=csgo-information"
+          rel="noreferrer"
+          target="_blank"
+          className="text-sm text-purple-700 hover:text-yellow-600 hover:no-underline"
+        >
+          by @richellyitalo
+        </a>
+      </p>
+    </>
   );
 }
